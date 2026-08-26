07903f8d88113aeb6fa60d59d1e8d7c724764f99
Mijat Krivokapić <mijat2004@gmail.com>
2026-08-26 16:57:37 +0200

    chore: initialize project

---
+++ backend/src/main/java/com/example/book2quiz/Book2quizApplication.java
@@ -0,0 +1,13 @@
+package com.example.book2quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book2quizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Book2quizApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/book2quiz/Book2quizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book2quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book2quizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
