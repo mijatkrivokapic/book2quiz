e6c1767edbd73a219c1fa708e68ba256d4a71b0a
Mijat Krivokapić <mijat2004@gmail.com>
2026-08-27 08:57:26 +0200

    chore: add model mapper config

---
+++ backend/src/main/java/com/example/book2quiz/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.example.book2quiz.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.record.RecordModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.registerModule(new RecordModule());
+        return modelMapper;
+    }
+}
